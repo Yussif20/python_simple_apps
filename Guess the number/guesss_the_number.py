c02a0c16fb16e882e7b80826db7ccaeb7a9f275b
Yousef <yousefayman2000500@gmail.com>
2024-06-08 01:23:14 +0300

    fixing the fixed number error

--- Guess the number/guesss_the_number.py
+++ Guess the number/guesss_the_number.py
@@ -3,38 +3,50 @@ import random
 attempts_list =[]
 attempts=0
 random_num=random.randint(1,10)
-print(random_num)
 
 def show_score():
     if not attempts_list:
         print("there is no currently a high score")
     else: print (f"the current high score is {min(attempts_list)} attempts")
-def attempts_increment():
-     global attempts
-     attempts = attempts + 1
-     attempts_list.append(attempts)
 
 print('Hello champ, welcome to the guessing game')
 player_name=input("What's your name ? ")
 wanna_play = input(f'Hi {player_name},would you like to play the game? (yes/no) ').lower()
 
-if wanna_play == "yes":
+if wanna_play == "no":
     print("cool")
-    while wanna_play=="yes":
-        guess=int(input("please choose a number between 1 and 10: "))
-        if (guess < 1 or guess > 10):
-            raise ValueError("please enter a number within the given range")
-        else :
-            if guess == random_num:    
-                print("Great! you Won")
-                attempts_increment()
-                show_score()
-                break
-            else : 
-                print("wrong, guess again")
-                attempts_increment()
     exit()
 else: show_score()
+
+while wanna_play=="yes":
+        try:
+            guess=int(input("please choose a number between 1 and 10: "))
+            if (guess < 1 or guess > 10):
+                raise ValueError("please enter a number within the given range")
+            elif guess == random_num:   
+                    print("Great! you Won")
+                    # attempts_increment()
+                    attempts = attempts + 1
+                    attempts_list.append(attempts)
+                    print(f'it took you {attempts} attempts')
+                    wanna_play=input("would you like to play again? (yes/no)").lower()
+                    if wanna_play=="no":
+                        print("cool, have a nice day")
+                    else :
+                        attempts = 0
+                        show_score()
+                        random_num=random.randint(1,10)
+                        continue
+            elif guess < random_num : 
+                    attempts = attempts + 1
+                    print("wrong, try higher")
+                    # attempts_increment()
+            else : 
+                    attempts = attempts + 1
+                    print("wrong, try lower")
+        except ValueError as err:
+            print(err)
+
 
 
    
